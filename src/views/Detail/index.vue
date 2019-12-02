a33ebc256dca71c37059b628115d694cd68bbaff
517282781 <517282781@qq.com>
2019-12-02 23:10:45 +0800

    detail header update

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
   </div>
 </template>
 
 <script>
 import DetailBanner from './subs/Banner'
+import DetailHeader from './subs/Header'
 
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
