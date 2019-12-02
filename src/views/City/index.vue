6f6ee4d2cd628bdac14497530e4aedb43b615b0b
517282781 <517282781@qq.com>
2019-12-02 20:14:31 +0800

    list update

--- src/views/City/index.vue
+++ src/views/City/index.vue
@@ -2,18 +2,24 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 
 <script>
 import CityHeader from './subs/Header'
 import CitySearch from './subs/Search'
+import CityList from './subs/List'
+import CityAlphabet from './subs/Alphabet'
 
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAlphabet
   }
 }
 </script>
