17971a3121288941d0876504438230d2c1666498
517282781 <517282781@qq.com>
2019-12-02 23:25:34 +0800

    list  update

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -2,18 +2,28 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
+    <div class="content">
+      <detail-list></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './subs/Banner'
 import DetailHeader from './subs/Header'
+import DetailList from './subs/List'
 
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data() {
+    return {
+      list: []
+    }
   }
 }
 </script>
